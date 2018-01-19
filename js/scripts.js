b84796a4da073a71d3bc022e1cee791b58b47135
Cameron Anderson <chamburg321@gmail.com>
2018-01-19 11:03:14 -0800

    add Dave functionlity

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,42 @@
 
-function doTheBeep(number) {
-	if (number.includes("0"))
-		number = "Beep";
-	return number;
+function checkForBeep(number) {
+	console.log(number.toString().includes("1"));
+	if (number.toString().includes("0"))
+		return true;
+	return false;
 }
 
-function doTheBoop(number) {
-	if (number.includes("1"))
-		number = "Boop"
-	return number;
+function checkForBoop(number) {
+	if (number.toString().includes("1"))
+		return true;
+	return false;
+}
+
+function checkForDave(number) {
+	if (number % 3 === 0)
+		return true;
+	return false;
 }
 
 function processNumber(number) {
-	number = doTheBeep(number);
-	number = doTheBoop(number);
-	return number;
+	var output = number.toString();
+	if (checkForBeep(number))
+		output = "Beep";
+	if (checkForBoop(number))
+		output = "Boop";
+	if (checkForDave(number))
+		output = "I'm sorry, Dave. I'm afraid I can't do that."
+	return output;
 }
 
 function processNumbers(max) {
 	var output = [];
 	max = parseInt(max);
-	if(isNaN(max)) {
+	if(isNaN(parseInt(max)) {
 		alert("That's not a number...");
 	} else {
 		for (var i = 0; i <= max; i+=1) {
-			output.push(processNumber(i + ""));
+			output.push(processNumber(i));
 		}
 	}
 	return output;
